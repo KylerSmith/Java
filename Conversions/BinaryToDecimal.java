1cb4dbb65f17c91c9d3d918e2e2b0149dd961f4d
KylerSmith <Kyler@macBookAir.local>
2017-07-13 19:31:37 -0700

    Made changes to BinaryToDecimal.java for readability

--- Conversions/BinaryToDecimal.java
+++ Conversions/BinaryToDecimal.java
@@ -1,33 +1,43 @@
+/**
+ * This class converts a Binary number to a Decimal.
+ *
+ * @author Unknown
+ * Modified 07/13/2017, Kyler Smith
+ */
 import java.util.Scanner;
 
-/**
- * This class converts a Binary number to a Decimal number
- * 
- * @author Unknown
- *
- */
-class BinaryToDecimal
-{
-	
+public class BinaryToDecimal {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int num, converted = 0;
+		System.out.print("Enter binary number: ");
+		num = scan.nextInt();
+
+		converted = convert(num);
+
+		System.out.println("Decimal equivalent: " + converted);
+		scan.close();
+	}
+
 	/**
-	 * Main Method
-	 * 
-	 * @param args Command line arguments
+	 * Function to perform convert
+	 *
+	 * @param binNum: Binary number to convert to Decimal
+	 * @return int
 	 */
-  public static void main(String args[])
-  {
-    Scanner sc=new Scanner(System.in);
-    int n,k,d,s=0,c=0;
-    System.out.print("Binary number: ");
-    n=sc.nextInt();
-    k=n;
-    while(k!=0)
-    {
-      d=k%10;
-      s+=d*(int)Math.pow(2,c++);
-      k/=10;
-    }
-    System.out.println("Decimal equivalent:"+s);
-    sc.close();
-  }
+	public static int convert(int binNum) {
+
+		int converted = 0, power = 0, lsb;
+
+		while(binNum != 0) {
+			lsb = binNum % 10;
+			converted += lsb * (int) Math.pow(2, power++);
+			binNum /= 10;
+		}
+
+		return converted;
+	}
+
 }
